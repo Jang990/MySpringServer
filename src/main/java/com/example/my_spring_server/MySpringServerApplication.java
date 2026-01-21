fd436dde1d6de59c35f43d6ed40177edbeef160d
Jang <sdsd090811@gmail.com>
2026-01-21 19:28:17 +0900

    feat: First Commit

---
+++ src/main/java/com/example/my_spring_server/MySpringServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.my_spring_server;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class MySpringServerApplication {
+
+	public static void main(String[] args) {
+		/*SpringApplication.run(MySpringServerApplication.class, args);*/
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/example/my_spring_server/MySpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_spring_server;
+
+import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class MySpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
