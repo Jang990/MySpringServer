469d83b1af34237a6689eefe66ea70b34b4e32f1
Jang <sdsd090811@gmail.com>
2026-01-27 19:41:23 +0900

    refactor: user save에 jdbctemplate 적용

--- src/main/java/com/example/my_spring_server/users/infra/UsersRepository.java
+++ src/main/java/com/example/my_spring_server/users/infra/UsersRepository.java
@@ -3,6 +3,7 @@ package com.example.my_spring_server.users.infra;
 import com.example.my_spring_server.DBConfig;
 import com.example.my_spring_server.jpa.MyEntityIdInjector;
 import com.example.my_spring_server.my.jdbctemplate.MyJdbcTemplate;
+import com.example.my_spring_server.my.jdbctemplate.MyKeyHolder;
 import com.example.my_spring_server.users.domain.Users;
 
 import java.sql.*;
@@ -18,22 +19,26 @@ public class UsersRepository {
 
     public Users save(Users users) {
         try(Connection conn = DriverManager.getConnection(dbConfig.getUrl(), dbConfig.getUsername(), dbConfig.getPassword());
-            PreparedStatement ps = conn.prepareStatement("""
-                    Insert Into Users (name, balance)
-                    values
-                    (? ,?)
-                    """, Statement.RETURN_GENERATED_KEYS); // 플레그 전달 - 생성된 ID 반환
-        ) {
-            ps.setString(1, users.getName());
-            ps.setInt(2, users.getBalance());
-            ps.executeUpdate();
 
-            try(ResultSet rs = ps.getGeneratedKeys()) {
-                boolean hasId = rs.next();
-                long id = rs.getLong(1); // id 반환
-                MyEntityIdInjector.injectId(users, id);
-                return users;
-            }
+        ) {
+            MyKeyHolder myKeyHolder = new MyKeyHolder();
+            myJdbcTemplate.update(
+                    conn,
+                    con -> {
+                        PreparedStatement ps = conn.prepareStatement("""
+                                Insert Into Users (name, balance)
+                                values
+                                (? ,?)
+                                """, Statement.RETURN_GENERATED_KEYS); // 플레그 전달 - 생성된 ID 반환
+                        ps.setString(1, users.getName());
+                        ps.setInt(2, users.getBalance());
+
+                        return ps;
+                    }, myKeyHolder
+            );
+
+            MyEntityIdInjector.injectId(users, myKeyHolder.getId());
+            return users;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
