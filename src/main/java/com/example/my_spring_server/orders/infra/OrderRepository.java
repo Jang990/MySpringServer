60095eba02a9869a3ac218fc3bfd50682267c721
Jang <sdsd090811@gmail.com>
2026-01-27 20:26:49 +0900

    refactor: order 저장 과정을 jdbctemplate으로 변경

--- src/main/java/com/example/my_spring_server/orders/infra/OrderRepository.java
+++ src/main/java/com/example/my_spring_server/orders/infra/OrderRepository.java
@@ -4,6 +4,7 @@ import com.example.my_spring_server.DBConfig;
 import com.example.my_spring_server.jpa.MyEntityIdInjector;
 import com.example.my_spring_server.my.jdbctemplate.MyJdbcTemplate;
 import com.example.my_spring_server.my.jdbctemplate.MyKeyHolder;
+import com.example.my_spring_server.my.jdbctemplate.MyRowMapper;
 import com.example.my_spring_server.orders.domain.OrderItems;
 import com.example.my_spring_server.orders.domain.Orders;
 
@@ -15,6 +16,37 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class OrderRepository {
+    private static final MyRowMapper<Orders> ordersMyRowMapper = rs -> {
+        Long orderId = null;
+        long userId = -1;
+        int totalPrice = -1;
+        LocalDateTime createdAt = null;
+        List<OrderItems> orderItems = new LinkedList<>();
+
+        while (true) {
+            if (orderId == null) {
+                orderId = rs.getLong(1);
+                userId = rs.getLong(2);
+                totalPrice = rs.getInt(3);
+                createdAt = rs.getObject(4, LocalDateTime.class);
+            }
+
+            long orderItemId = rs.getLong(5);
+            long foodId = rs.getLong(6);
+            int priceAtOrder = rs.getInt(7);
+            int quantity = rs.getInt(8);
+
+            // 기본 생성자로 생성
+            OrderItems orderItem = createOrderItemWithReflection(orderItemId, orderId, foodId, priceAtOrder, quantity);
+            orderItems.add(orderItem);
+
+            if(!rs.next())
+                break;
+        }
+
+        return createOrderWithReflection(orderId, userId, totalPrice, createdAt, orderItems);
+    };
+
     private final DBConfig dbConfig;
     private final MyJdbcTemplate myJdbcTemplate;
 
@@ -46,77 +78,50 @@ public class OrderRepository {
     }
 
     public Orders findById(long id) {
-        try (Connection conn = DriverManager.getConnection(dbConfig.getUrl(), dbConfig.getUsername(), dbConfig.getPassword());
-                PreparedStatement ps = conn.prepareStatement("""
-                        SELECT o.id, o.user_id, o.total_price, o.created_at, oi.id, oi.food_id, oi.price_at_order, oi.quantity
-                        FROM orders o
-                        LEFT OUTER JOIN order_items oi ON o.id = oi.order_id
-                        WHERE o.id = ?
-                        """)) {
-            ps.setLong(1, id);
-
-            try (ResultSet rs = ps.executeQuery()) {
-                Long orderId = null;
-                long userId = -1;
-                int totalPrice = -1;
-                LocalDateTime createdAt = null;
-                List<OrderItems> orderItems = new LinkedList<>();
-
-                boolean hasNext = rs.next();
-                if(!hasNext)
-                    throw new IllegalArgumentException("주문을 찾을 수 없습니다.");
-
-                while (hasNext) {
-                    if (orderId == null) {
-                        orderId = rs.getLong(1);
-                        userId = rs.getLong(2);
-                        totalPrice = rs.getInt(3);
-                        createdAt = rs.getObject(4, LocalDateTime.class);
-                    }
-
-                    long orderItemId = rs.getLong(5);
-                    long foodId = rs.getLong(6);
-                    int priceAtOrder = rs.getInt(7);
-                    int quantity = rs.getInt(8);
-
-                    // 기본 생성자로 생성
-                    OrderItems orderItem = createOrderItemWithReflection(orderItemId, orderId, foodId, priceAtOrder, quantity);
-                    orderItems.add(orderItem);
-                    hasNext = rs.next();
-                }
-
-                return createOrderWithReflection(orderId, userId, totalPrice, createdAt, orderItems);
-            }
-        } catch (SQLException | NoSuchMethodException | InstantiationException | IllegalAccessException |
-                 InvocationTargetException e) {
+        try (Connection conn = DriverManager.getConnection(dbConfig.getUrl(), dbConfig.getUsername(), dbConfig.getPassword())) {
+            return myJdbcTemplate.queryForObject(conn, """
+                    SELECT o.id, o.user_id, o.total_price, o.created_at, oi.id, oi.food_id, oi.price_at_order, oi.quantity
+                    FROM orders o
+                    LEFT OUTER JOIN order_items oi ON o.id = oi.order_id
+                    WHERE o.id = ?
+                    """, ordersMyRowMapper, id);
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private Orders createOrderWithReflection(Long orderId, long userId, int totalPrice, LocalDateTime createdAt, List<OrderItems> orderItems) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        Constructor<Orders> defaultConstructor = Orders.class.getDeclaredConstructor();
-        defaultConstructor.setAccessible(true);
-        Orders order = defaultConstructor.newInstance();
+    private static Orders createOrderWithReflection(Long orderId, long userId, int totalPrice, LocalDateTime createdAt, List<OrderItems> orderItems) {
+        try {
+            Constructor<Orders> defaultConstructor = Orders.class.getDeclaredConstructor();
+            defaultConstructor.setAccessible(true);
+            Orders order = defaultConstructor.newInstance();
 
-        MyEntityIdInjector.injectId(order, "id", orderId);
-        MyEntityIdInjector.injectId(order, "userId", userId);
-        MyEntityIdInjector.injectId(order, "totalPrice", totalPrice);
-        MyEntityIdInjector.injectId(order, "createdAt", createdAt);
-        MyEntityIdInjector.injectId(order, "orderItems", orderItems);
-        return order;
+            MyEntityIdInjector.injectId(order, "id", orderId);
+            MyEntityIdInjector.injectId(order, "userId", userId);
+            MyEntityIdInjector.injectId(order, "totalPrice", totalPrice);
+            MyEntityIdInjector.injectId(order, "createdAt", createdAt);
+            MyEntityIdInjector.injectId(order, "orderItems", orderItems);
+            return order;
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private OrderItems createOrderItemWithReflection(long orderItemId, Long orderId, long foodId, int priceAtOrder, int quantity) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
-        Constructor<OrderItems> defaultConstructor = OrderItems.class.getDeclaredConstructor();
-        defaultConstructor.setAccessible(true);
-        OrderItems orderItem = defaultConstructor.newInstance();
+    private static OrderItems createOrderItemWithReflection(long orderItemId, Long orderId, long foodId, int priceAtOrder, int quantity) {
+        try {
+            Constructor<OrderItems> defaultConstructor = OrderItems.class.getDeclaredConstructor();
+            defaultConstructor.setAccessible(true);
+            OrderItems orderItem = defaultConstructor.newInstance();
 
-        MyEntityIdInjector.injectId(orderItem, "id", orderItemId);
-        MyEntityIdInjector.injectId(orderItem, "orderId", orderId);
-        MyEntityIdInjector.injectId(orderItem, "foodId", foodId);
-        MyEntityIdInjector.injectId(orderItem, "priceAtOrder", priceAtOrder);
-        MyEntityIdInjector.injectId(orderItem, "quantity", quantity);
-        return orderItem;
+            MyEntityIdInjector.injectId(orderItem, "id", orderItemId);
+            MyEntityIdInjector.injectId(orderItem, "orderId", orderId);
+            MyEntityIdInjector.injectId(orderItem, "foodId", foodId);
+            MyEntityIdInjector.injectId(orderItem, "priceAtOrder", priceAtOrder);
+            MyEntityIdInjector.injectId(orderItem, "quantity", quantity);
+            return orderItem;
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // Order 삽입
