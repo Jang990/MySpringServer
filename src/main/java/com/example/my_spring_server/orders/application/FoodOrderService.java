3c3cd7fd9286e9bc4c1d78fae956a5d4bdc6d5c0
Jang <sdsd090811@gmail.com>
2026-01-31 16:00:34 +0900

    feat: Connection을 제거하고 트랜잭션 매니저를 사용하는 FoodOrderService 초기 코드 추가

--- src/main/java/com/example/my_spring_server/orders/application/FoodOrderService.java
+++ src/main/java/com/example/my_spring_server/orders/application/FoodOrderService.java
@@ -4,6 +4,7 @@ import com.example.my_spring_server.foods.domain.FoodOrders;
 import com.example.my_spring_server.foods.domain.Foods;
 import com.example.my_spring_server.foods.infra.FoodsRepository;
 import com.example.my_spring_server.my.datasource.MyDataSource;
+import com.example.my_spring_server.my.transaction.MyTransactionManager;
 import com.example.my_spring_server.orders.domain.OrderService;
 import com.example.my_spring_server.orders.domain.Orders;
 import com.example.my_spring_server.orders.infra.OrderRepository;
@@ -11,8 +12,6 @@ import com.example.my_spring_server.orders.presentation.dto.FoodOrderRequests;
 import com.example.my_spring_server.users.domain.Users;
 import com.example.my_spring_server.users.infra.UsersRepository;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
 public class FoodOrderService {
@@ -23,18 +22,22 @@ public class FoodOrderService {
     private final FoodsRepository foodsRepository;
     private final UsersRepository usersRepository;
 
+    private final MyTransactionManager myTransactionManager;
+
     public FoodOrderService(
             MyDataSource myDataSource,
             OrderService orderService,
             OrderRepository orderRepository,
             FoodsRepository foodsRepository,
-            UsersRepository usersRepository
+            UsersRepository usersRepository,
+            MyTransactionManager myTransactionManager
     ) {
         this.myDataSource = myDataSource;
         this.orderService = orderService;
         this.orderRepository = orderRepository;
         this.foodsRepository = foodsRepository;
         this.usersRepository = usersRepository;
+        this.myTransactionManager = myTransactionManager;
     }
 
 
@@ -43,22 +46,17 @@ public class FoodOrderService {
         List<Foods> foods = foodsRepository.findAll(foodOrderRequests.foodIds());
         List<FoodOrders> foodOrders = FoodOrders.from(foodOrderRequests, foods);
 
-        try(Connection conn = myDataSource.getConnection()) {
-            try {
-                conn.setAutoCommit(false);
-                Orders order = orderService.order(user, foodOrders);
+        myTransactionManager.startTransaction();
+        try {
+            Orders order = orderService.order(user, foodOrders);
+            orderRepository.save(order);
+            usersRepository.updateBalance(user.getId(), user.getBalance());
+            for (Foods food : foods)
+                foodsRepository.updateStock(food.getId(), food.getStock());
 
-                orderRepository.save(conn, order);
-                usersRepository.updateBalance(conn, user.getId(), user.getBalance());
-                for (Foods food : foods)
-                    foodsRepository.updateStock(conn, food.getId(), food.getStock());
-                conn.commit();
-            } catch (SQLException | RuntimeException e) {
-                conn.rollback();
-                throw e;
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            myTransactionManager.commit();
+        } catch(RuntimeException e) {
+            myTransactionManager.rollback();
         }
     }
 }
