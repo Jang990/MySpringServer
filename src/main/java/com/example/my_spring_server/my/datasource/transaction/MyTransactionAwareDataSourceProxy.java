6355ea1b06bbdeed35ed845d1767e06e8cc280aa
Jang <sdsd090811@gmail.com>
2026-02-03 18:57:12 +0900

    feat: Jdk Dynamic Proxy를 활용한 Connection 프록시 생성
    트랜잭션 Connection이 존재하면 DataSource에서 받은 Connection으로 close를 호출해도 동작하지 않도록 설정.
    => Connection close는 트랜잭션 매니저가 관리함

--- src/main/java/com/example/my_spring_server/my/datasource/transaction/MyTransactionAwareDataSourceProxy.java
+++ src/main/java/com/example/my_spring_server/my/datasource/transaction/MyTransactionAwareDataSourceProxy.java
@@ -3,6 +3,7 @@ package com.example.my_spring_server.my.datasource.transaction;
 import com.example.my_spring_server.my.datasource.MyDataSource;
 import com.example.my_spring_server.my.transaction.MyConnectionThreadLocal;
 
+import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -19,6 +20,21 @@ public class MyTransactionAwareDataSourceProxy implements MyDataSource {
         if(txConn == null)
             return delegate.getConnection();
         else
-            return txConn;
+            return suppressClose(txConn);
+    }
+
+    // Connection의 conn.close()가 동작하지 않도록 프록시 객체를 반환
+    private Connection suppressClose(Connection conn) {
+        // Proxy.newProxyInstance(loader, interfaces, handler) = 인터페이스 구현 프록시 객체를 생성
+        return (Connection) Proxy.newProxyInstance(
+                conn.getClass().getClassLoader(),
+                new Class[]{Connection.class},
+                (proxy, method, args) -> {
+                    // close 메소드 호출이 오면 아무 동작도 하지 않음
+                    if ("close".equals(method.getName()))
+                        return null;
+                    return method.invoke(conn, args); // 다른 메소드들은 conn 메소드로 정상 동작
+                }
+        );
     }
 }
