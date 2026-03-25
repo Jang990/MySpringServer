9550370c3f2241eaf4499ca2ed4a2bcda5296617
Jang <sdsd090811@gmail.com>
2026-03-25 19:11:09 +0900

    feat: foods 패키지 하위 컴포넌트들을 빈으로 등록

--- src/main/java/com/example/my_spring_server/foods/infra/FoodsRepository.java
+++ src/main/java/com/example/my_spring_server/foods/infra/FoodsRepository.java
@@ -6,11 +6,13 @@ import com.example.my_spring_server.my.jdbctemplate.EmptyResultException;
 import com.example.my_spring_server.my.jdbctemplate.MyJdbcTemplate;
 import com.example.my_spring_server.my.jdbctemplate.MyKeyHolder;
 import com.example.my_spring_server.my.jdbctemplate.MyRowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.*;
 import java.util.Collections;
 import java.util.List;
 
+@Component
 public class FoodsRepository {
     private final MyJdbcTemplate myJdbcTemplate;
 
