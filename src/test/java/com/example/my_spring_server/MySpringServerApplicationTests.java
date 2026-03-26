7ea42756554052967aea6e1427bbcebed3023d1f
Jang <sdsd090811@gmail.com>
2026-03-26 17:18:57 +0900

    test: IoC 컨테이너 테스트 코드 추가

--- src/test/java/com/example/my_spring_server/MySpringServerApplicationTests.java
+++ src/test/java/com/example/my_spring_server/MySpringServerApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.my_spring_server;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 //@SpringBootTest
 class MySpringServerApplicationTests {
 
 	@Test
+	@DisplayName("IoC 컨테이너 테스트")
 	void contextLoads() {
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 	}
 
 }
